98141739e9539b5ff7ada37c42326d537c723b36
parviz <zulfugarov@arnica.pro>
2023-07-06 10:54:02 +0500

    Add default settings for solving any test task

--- config/console.php
+++ config/console.php
@@ -9,9 +9,7 @@ $config = [
     'bootstrap' => [
         'queue',
         'log',
-        'redis',
-        'elasticsearch',
-        'cache'
+        'redis'
     ],
     'controllerNamespace' => 'app\commands',
     'aliases' => [
@@ -45,27 +43,6 @@ $config = [
             'port' => $_ENV['REDIS_PORT'],
             'database' => 0,
             'password' => $_ENV['REDIS_PASSWORD']
-        ],
-        'elasticsearch' => [
-            'class' => 'yii\elasticsearch\Connection',
-            'nodes' => [
-                ['http_address' => 'elasticsearch:9200'],
-                // configure more hosts if you have a cluster
-            ],
-            // set autodetectCluster to false if you don't want to auto detect nodes
-            // 'autodetectCluster' => false,
-            'dslVersion' => 7, // default is 5
-        ],
-        'cache' => [
-            'class'        => 'yii\caching\MemCache',
-            'useMemcached' => true,
-            'servers' => [
-                [
-                    'host' => 'memcached',
-                    'port' => 11211,
-                    'weight' => 60,
-                ]
-            ],
         ],
     ],
     'params' => $params,
